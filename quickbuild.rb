89ba38c931e08ffde3f12330cb770375e0a1f2ab
Ryan Dowell <ssstarduster+github@gmail.com>
2012-07-10 23:58:08 -0500

    Properly locate statemachine.rb

--- quickbuild.rb
+++ quickbuild.rb
@@ -33,7 +33,16 @@
 VERSION='2.10'
 
 require 'optparse'
-require './statemachine.rb'
+
+# A patch for Ruby versions less than 1.9.2
+unless Kernel.respond_to?(:require_relative)
+	module Kernel
+		def require_relative(path)
+			require File.join(File.dirname(caller[0]), path.to_str)
+		end
+	end
+end
+require_relative 'statemachine.rb'
 
 
 # Program process:
