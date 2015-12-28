3166144158f500cc3b949843a8de5d92fed68b0f
Ryan Dowell <ssstarduster+github@gmail.com>
2015-12-28 16:06:36 -0600

    Fix tests by replacing global SYNTAXP with constructed object

--- processors.rb
+++ processors.rb
@@ -55,119 +55,128 @@ end
 # Most state modifiers here return the current_state as the new_state,
 # to preserve values in current_state.
 
-SYNTAXP = StateMachine.new(:default)
-SYNTAXP.push Action.new(/^\s*$/,
-	[:default, lambda{|s| [s,[[:NOP]] ]}],
-	[:in,      lambda{|s| [s,[[:NOP]] ]}],
-	[:on,      lambda{|s| [s,[[:NOP]] ]}] )
-SYNTAXP.push Action.new(/^@@/,
-	[:in, lambda{|s| [s,[[:NOP]] ]}],
-	[:on, lambda{|s| [s,[[:NOP]] ]}] )
+class InputStateMachine
+  attr_reader :machine
+  def initialize
+    @machine = StateMachine.new(:default)
+    @machine.push Action.new(/^\s*$/,
+      [:default, lambda{|s| [s,[[:NOP]] ]}],
+      [:in,      lambda{|s| [s,[[:NOP]] ]}],
+      [:on,      lambda{|s| [s,[[:NOP]] ]}] )
+    @machine.push Action.new(/^@@/,
+      [:in, lambda{|s| [s,[[:NOP]] ]}],
+      [:on, lambda{|s| [s,[[:NOP]] ]}] )
 
-closebracket = lambda {|s,input,e|
-	str = (s[:bracketline] == e[:linenumber] - 1) ? '%r' : ''
-	str += buffer_escape(input.sub(/^>/,''))
-	command = [[:BUFFER_ROOM, s[:roomname], str]] if s[:state] == :IN
-	command = [[:BUFFER_EXIT, s[:roomname], s[:exitname], str]] if s[:state] == :ON
-	return [s.merge({:bracketline => e[:linenumber]}), command]
-}
-SYNTAXP.push Action.new(/^>/,
-	[:in, closebracket],
-	[:on, closebracket])
+    closebracket = lambda {|s,input,e|
+      str = (s[:bracketline] == e[:linenumber] - 1) ? '%r' : ''
+      str += buffer_escape(input.sub(/^>/,''))
+      command = [[:BUFFER_ROOM, s[:roomname], str]] if s[:state] == :IN
+      command = [[:BUFFER_EXIT, s[:roomname], s[:exitname], str]] if s[:state] == :ON
+      return [s.merge({:bracketline => e[:linenumber]}), command]
+    }
+    @machine.push Action.new(/^>/,
+      [:in, closebracket],
+      [:on, closebracket])
 
-SYNTAXP.push Action.new(/^#.*$/,
-	[:default, lambda {|s| [s, [[:NOP]]]}],
-	[:in,      lambda {|s,i,e| [s, [[:BUFFER_ROOM, s[:roomname], buffer_prefix(e[:matchdata][0])]] ]}],
-	[:on,      lambda {|s,i,e| [s, [[:BUFFER_EXIT, s[:roomname], s[:exitname], buffer_prefix(e[:matchdata][0])]] ]}] )
+    @machine.push Action.new(/^#.*$/,
+      [:default, lambda {|s| [s, [[:NOP]]]}],
+      [:in,      lambda {|s,i,e| [s, [[:BUFFER_ROOM, s[:roomname], buffer_prefix(e[:matchdata][0])]] ]}],
+      [:on,      lambda {|s,i,e| [s, [[:BUFFER_EXIT, s[:roomname], s[:exitname], buffer_prefix(e[:matchdata][0])]] ]}] )
 
-SYNTAXP.push ActionWIND.new(/^ATTR BASE:\s*(.*)$/,
-	[:default, lambda {|s,i,e| [s, [[:ATTR_BASE, e[:matchdata][1]]] ]}] )
+    @machine.push ActionWIND.new(/^ATTR BASE:\s*(.*)$/,
+      [:default, lambda {|s,i,e| [s, [[:ATTR_BASE, e[:matchdata][1]]] ]}] )
 
-SYNTAXP.push ActionWIND.new(/^ALIAS\s*:?\s*(".*")\s*"(.*)"\s*$/i,
-	[:default, lambda {|s,i,e| [s, [[:ALIAS, e[:matchdata][1], e[:matchdata][2]]] ]}] )
+    @machine.push ActionWIND.new(/^ALIAS\s*:?\s*(".*")\s*"(.*)"\s*$/i,
+      [:default, lambda {|s,i,e| [s, [[:ALIAS, e[:matchdata][1], e[:matchdata][2]]] ]}] )
 
-SYNTAXP.push ActionWIND.new(/^REVERSE\s*:?\s*(".*")\s*(".*")\s*$/i,
-	[:default, lambda {|s,i,e| [s, [[:REVERSE, e[:matchdata][1], e[:matchdata][2]]] ]}] )
+    @machine.push ActionWIND.new(/^REVERSE\s*:?\s*(".*")\s*(".*")\s*$/i,
+      [:default, lambda {|s,i,e| [s, [[:REVERSE, e[:matchdata][1], e[:matchdata][2]]] ]}] )
 
-SYNTAXP.push ActionWIND.new(/^ROOM PARENT:\s*$/,
-	[:default, lambda {|s,i,e| [s, [[:ROOM_PARENT, nil, nil]] ]}] )
-SYNTAXP.push ActionWIND.new(/^ROOM PARENT:\s*(#\d+)\s*$/,
-	[:default, lambda {|s,i,e| [s, [[:ROOM_PARENT, e[:matchdata][1], :raw]] ]}] )
-SYNTAXP.push ActionWIND.new(/^ROOM PARENT:\s*(".*"(?:[^->\s]\S*)?)\s*$/,
-	[:default, lambda {|s,i,e| [s, [[:ROOM_PARENT, e[:matchdata][1], :id]] ]}] )
+    @machine.push ActionWIND.new(/^ROOM PARENT:\s*$/,
+      [:default, lambda {|s,i,e| [s, [[:ROOM_PARENT, nil, nil]] ]}] )
+    @machine.push ActionWIND.new(/^ROOM PARENT:\s*(#\d+)\s*$/,
+      [:default, lambda {|s,i,e| [s, [[:ROOM_PARENT, e[:matchdata][1], :raw]] ]}] )
+    @machine.push ActionWIND.new(/^ROOM PARENT:\s*(".*"(?:[^->\s]\S*)?)\s*$/,
+      [:default, lambda {|s,i,e| [s, [[:ROOM_PARENT, e[:matchdata][1], :id]] ]}] )
 
-SYNTAXP.push ActionWIND.new(/^ROOM ZONE:\s*$/,
-	[:default, lambda {|s,i,e| [s, [[:ROOM_ZONE, nil, nil]] ]}] )
-SYNTAXP.push ActionWIND.new(/^ROOM ZONE:\s*(#\d+)\s*$/,
-	[:default, lambda {|s,i,e| [s, [[:ROOM_ZONE, e[:matchdata][1], :raw]] ]}] )
-SYNTAXP.push ActionWIND.new(/^ROOM ZONE:\s*(".*"(?:[^->\s]\S*)?)\s*$/,
-	[:default, lambda {|s,i,e| [s, [[:ROOM_ZONE, e[:matchdata][1], :id]] ]}] )
+    @machine.push ActionWIND.new(/^ROOM ZONE:\s*$/,
+      [:default, lambda {|s,i,e| [s, [[:ROOM_ZONE, nil, nil]] ]}] )
+    @machine.push ActionWIND.new(/^ROOM ZONE:\s*(#\d+)\s*$/,
+      [:default, lambda {|s,i,e| [s, [[:ROOM_ZONE, e[:matchdata][1], :raw]] ]}] )
+    @machine.push ActionWIND.new(/^ROOM ZONE:\s*(".*"(?:[^->\s]\S*)?)\s*$/,
+      [:default, lambda {|s,i,e| [s, [[:ROOM_ZONE, e[:matchdata][1], :id]] ]}] )
 
-SYNTAXP.push ActionWIND.new(/^ROOM FLAGS:\s*$/,
-	[:default, lambda {|s,i,e| [s, [[:ROOM_FLAGS, nil]] ]}] )
-SYNTAXP.push ActionWIND.new(/^ROOM FLAGS:\s*(.*)\s*$/,
-	[:default, lambda {|s,i,e| [s, [[:ROOM_FLAGS, e[:matchdata][1]]] ]}] )
+    @machine.push ActionWIND.new(/^ROOM FLAGS:\s*$/,
+      [:default, lambda {|s,i,e| [s, [[:ROOM_FLAGS, nil]] ]}] )
+    @machine.push ActionWIND.new(/^ROOM FLAGS:\s*(.*)\s*$/,
+      [:default, lambda {|s,i,e| [s, [[:ROOM_FLAGS, e[:matchdata][1]]] ]}] )
 
-SYNTAXP.push ActionWIND.new(/^EXIT PARENT:\s*$/,
-	[:default, lambda {|s,i,e| [s, [[:EXIT_PARENT, nil, nil]] ]}] )
-SYNTAXP.push ActionWIND.new(/^EXIT PARENT:\s*(#\d+)\s*$/,
-	[:default, lambda {|s,i,e| [s, [[:EXIT_PARENT, e[:matchdata][1], :raw]] ]}] )
-SYNTAXP.push ActionWIND.new(/^EXIT PARENT:\s*(".*"(?:[^->\s]\S*)?)\s*$/,
-	[:default, lambda {|s,i,e| [s, [[:EXIT_PARENT, e[:matchdata][1], :id]] ]}] )
+    @machine.push ActionWIND.new(/^EXIT PARENT:\s*$/,
+      [:default, lambda {|s,i,e| [s, [[:EXIT_PARENT, nil, nil]] ]}] )
+    @machine.push ActionWIND.new(/^EXIT PARENT:\s*(#\d+)\s*$/,
+      [:default, lambda {|s,i,e| [s, [[:EXIT_PARENT, e[:matchdata][1], :raw]] ]}] )
+    @machine.push ActionWIND.new(/^EXIT PARENT:\s*(".*"(?:[^->\s]\S*)?)\s*$/,
+      [:default, lambda {|s,i,e| [s, [[:EXIT_PARENT, e[:matchdata][1], :id]] ]}] )
 
-SYNTAXP.push ActionWIND.new(/^EXIT ZONE:\s*$/,
-	[:default, lambda {|s,i,e| [s, [[:EXIT_ZONE, nil, nil]] ]}] )
-SYNTAXP.push ActionWIND.new(/^EXIT ZONE:\s*(#\d+)\s*$/,
-	[:default, lambda {|s,i,e| [s, [[:EXIT_ZONE, e[:matchdata][1], :raw]] ]}] )
-SYNTAXP.push ActionWIND.new(/^EXIT ZONE:\s*(".*"(?:[^->\s]\S*)?)\s*$/,
-	[:default, lambda {|s,i,e| [s, [[:EXIT_ZONE, e[:matchdata][1], :id]] ]}] )
+    @machine.push ActionWIND.new(/^EXIT ZONE:\s*$/,
+      [:default, lambda {|s,i,e| [s, [[:EXIT_ZONE, nil, nil]] ]}] )
+    @machine.push ActionWIND.new(/^EXIT ZONE:\s*(#\d+)\s*$/,
+      [:default, lambda {|s,i,e| [s, [[:EXIT_ZONE, e[:matchdata][1], :raw]] ]}] )
+    @machine.push ActionWIND.new(/^EXIT ZONE:\s*(".*"(?:[^->\s]\S*)?)\s*$/,
+      [:default, lambda {|s,i,e| [s, [[:EXIT_ZONE, e[:matchdata][1], :id]] ]}] )
 
-SYNTAXP.push ActionWIND.new(/^EXIT FLAGS:\s*$/,
-	[:default, lambda {|s,i,e| [s, [[:EXIT_FLAGS, nil]] ]}] )
-SYNTAXP.push ActionWIND.new(/^EXIT FLAGS:\s*(.*)\s*$/,
-	[:default, lambda {|s,i,e| [s, [[:EXIT_FLAGS, e[:matchdata][1]]] ]}] )
+    @machine.push ActionWIND.new(/^EXIT FLAGS:\s*$/,
+      [:default, lambda {|s,i,e| [s, [[:EXIT_FLAGS, nil]] ]}] )
+    @machine.push ActionWIND.new(/^EXIT FLAGS:\s*(.*)\s*$/,
+      [:default, lambda {|s,i,e| [s, [[:EXIT_FLAGS, e[:matchdata][1]]] ]}] )
 
-SYNTAXP.push ActionWIND.new(/^(".*?")\s*:\s*((".*?"(?:[^->\s]\S*)?)(\s*(<?->)\s*(".*?"(?:[^->\s]\S*)?))+)\s*$/,
-	[:default, lambda {|s,i,e|
-		exitname, roomstring = e[:matchdata][1], e[:matchdata][2]
-		lastroom = e[:matchdata][3]
-		commands = [[:CREATE_ROOM, lastroom]]
-		roomstring.scan(/\s*(<?->)\s*(".*?"(?:[^->\s]\S*)?)/).each {|match|
-			commands.push([:CREATE_ROOM, match[1]])
-			commands.push([:CREATE_EXIT, exitname, lastroom, match[1]])
-			commands.push([:CREATE_REVERSE_EXIT, exitname, lastroom, match[1]]) if match[0] == "<->"
-			lastroom = match[1]
-		}
-		return {:state => s, :action => commands}
-	}])
+    @machine.push ActionWIND.new(/^(".*?")\s*:\s*((".*?"(?:[^->\s]\S*)?)(\s*(<?->)\s*(".*?"(?:[^->\s]\S*)?))+)\s*$/,
+      [:default, lambda {|s,i,e|
+        exitname, roomstring = e[:matchdata][1], e[:matchdata][2]
+        lastroom = e[:matchdata][3]
+        commands = [[:CREATE_ROOM, lastroom]]
+        roomstring.scan(/\s*(<?->)\s*(".*?"(?:[^->\s]\S*)?)/).each {|match|
+          commands.push([:CREATE_ROOM, match[1]])
+          commands.push([:CREATE_EXIT, exitname, lastroom, match[1]])
+          commands.push([:CREATE_REVERSE_EXIT, exitname, lastroom, match[1]]) if match[0] == "<->"
+          lastroom = match[1]
+        }
+        return {:state => s, :action => commands}
+      }])
 
-SYNTAXP.push ActionWIND.new(/^IN\s+(".*"(?:[^->\s]\S*)?)\s*$/,
-	[:default, lambda {|s,i,e| [{:state => :in, :roomname => e[:matchdata][1]}, [[:NOP]] ]}] )
+    @machine.push ActionWIND.new(/^IN\s+(".*"(?:[^->\s]\S*)?)\s*$/,
+      [:default, lambda {|s,i,e| [{:state => :in, :roomname => e[:matchdata][1]}, [[:NOP]] ]}] )
 
-SYNTAXP.push ActionWIND.new(/^ON\s+(".*")\s+FROM\s+(".*"(?:[^->\s]\S*)?)\s*$/,
-	[:default, lambda {|s,i,e| [{:state => :on, :roomname => e[:matchdata][2], :exitname => e[:matchdata][1]}, [[:NOP]] ]}] )
+    @machine.push ActionWIND.new(/^ON\s+(".*")\s+FROM\s+(".*"(?:[^->\s]\S*)?)\s*$/,
+      [:default, lambda {|s,i,e| [{:state => :on, :roomname => e[:matchdata][2], :exitname => e[:matchdata][1]}, [[:NOP]] ]}] )
 
-SYNTAXP.push Action.new(/^ENDIN\s*$/,
-	[:in,      lambda {|s,i,e| [:default, [[:NOP]] ]}],
-	[:default, lambda {|s,i,e| [:error,   [[:ERROR, "ENDIN outside of IN-block."]] ]}],
-	[:on,      lambda {|s,i,e| [:default, [[:WARNING, "ENDIN inside ON-block."]] ]}] )
+    @machine.push Action.new(/^ENDIN\s*$/,
+      [:in,      lambda {|s,i,e| [:default, [[:NOP]] ]}],
+      [:default, lambda {|s,i,e| [:error,   [[:ERROR, "ENDIN outside of IN-block."]] ]}],
+      [:on,      lambda {|s,i,e| [:default, [[:WARNING, "ENDIN inside ON-block."]] ]}] )
 
-SYNTAXP.push Action.new(/^ENDON\s*$/,
-	[:on,      lambda {|s,i,e| [:default, [[:NOP]] ]}],
-	[:default, lambda {|s,i,e| [:error,   [[:ERROR, "ENDON outside of ON-block."]] ]}],
-	[:in,      lambda {|s,i,e| [:default, [[:WARNING, "ENDON inside IN-block."]] ]}] )
+    @machine.push Action.new(/^ENDON\s*$/,
+      [:on,      lambda {|s,i,e| [:default, [[:NOP]] ]}],
+      [:default, lambda {|s,i,e| [:error,   [[:ERROR, "ENDON outside of ON-block."]] ]}],
+      [:in,      lambda {|s,i,e| [:default, [[:WARNING, "ENDON inside IN-block."]] ]}] )
 
-SYNTAXP.push ActionWIND.new(/^DESC(?:RIBE)?\s+(".*?"(?:[^=->\s]\S*)?)\s*=\s*(.*)$/,
-	[:default, lambda {|s,i,e| [s, [[:BUFFER_ROOM, e[:matchdata][1], "\n@describe here=" + e[:matchdata][2]]] ]}] )
+    @machine.push ActionWIND.new(/^DESC(?:RIBE)?\s+(".*?"(?:[^=->\s]\S*)?)\s*=\s*(.*)$/,
+      [:default, lambda {|s,i,e| [s, [[:BUFFER_ROOM, e[:matchdata][1], "\n@describe here=" + e[:matchdata][2]]] ]}] )
 
-SYNTAXP.push Action.new(/^.+$/,
-	[:default, lambda {|s,i,e| [:error, [[:ERROR, "Unrecognized command: #{e[:matchdata][0]}"]] ]}],
-	[:in,      lambda {|s,i,e| [s, [[:BUFFER_ROOM, s[:roomname], buffer_prefix(e[:matchdata][0])]] ]}],
-	[:on,      lambda {|s,i,e| [s, [[:BUFFER_EXIT, s[:roomname], s[:exitname], buffer_prefix(e[:matchdata][0])]] ]}] )
+    @machine.push Action.new(/^.+$/,
+      [:default, lambda {|s,i,e| [:error, [[:ERROR, "Unrecognized command: #{e[:matchdata][0]}"]] ]}],
+      [:in,      lambda {|s,i,e| [s, [[:BUFFER_ROOM, s[:roomname], buffer_prefix(e[:matchdata][0])]] ]}],
+      [:on,      lambda {|s,i,e| [s, [[:BUFFER_EXIT, s[:roomname], s[:exitname], buffer_prefix(e[:matchdata][0])]] ]}] )
+
+    def invoke(*args)
+      @machine.invoke(*args)
+    end
+  end
+end
 
 
 def process_file(file)
-  parser = SYNTAXP
+  parser = InputStateMachine.new()
 	extras = {:linenumber => 0}
 	commands = []
 
